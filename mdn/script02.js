3e710a0de53bca6de067123d246af3682efd49af
Lyle Strain <lylestrain@gmail.com>
2024-01-03 08:54:27 -0500

    added lots of stuff

---
+++ mdn/script02.js
@@ -0,0 +1,23 @@
+// Examine the document object
+
+console.dir(document);
+
+//console.log(document.domain);
+//console.log(document.URL);
+
+//console.log(document.title);
+//document.title = '123ABC' ;
+
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+console.log(document.all);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.getElementById('mainHeader'));
+//let MainHeader = document.getElementById("mainHeader").innerHTML;
+//console.log(MainHeader);
+//let headingA = document.querySelector("mainHeader");
+let headingA = document.getElementById("mainHeader");
+console.log(headingA);
